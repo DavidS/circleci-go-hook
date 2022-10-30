e9dfed0bbc856974eb1e0865b24621a6cc654f9b
David Schmitt <david@black.co.at>
2022-10-30 19:34:09 +0000

    Add opentelemetry-go skelly

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/DavidS/circleci-go-hook
 
 go 1.19
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.11.1 // indirect
+	go.opentelemetry.io/otel/trace v1.11.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html"
-	"io"
 	"log"
 	"net/http"
+	"sync"
+
+	"go.opentelemetry.io/otel/trace"
 )
 
+type hookHandler struct {
+	mu     sync.Mutex
+	tracer trace.Tracer
+}
+
+func (h *hookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	_, span := h.tracer.Start(context.Background(), "request")
+	defer span.End()
+	defer fmt.Fprintf(w, "Created Request\n")
+}
+
 func main() {
-	h1 := func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello from a HandleFunc #1!\n")
-	}
-	h2 := func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello from a HandleFunc #2!\n")
-	}
-	http.HandleFunc("/foo", h1)
-	http.HandleFunc("/", h2)
+	http.Handle("", new(hookHandler))
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
